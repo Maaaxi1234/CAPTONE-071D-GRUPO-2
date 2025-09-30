25cf7d597fc129671e4f75249087049eae063ad6
Roni Saavedra <ronisaavedra777@gmail.com>
2025-09-30 20:56:36 -0300

    Creación del CRUD

--- Fase 2/Evidencias del proyecto/punto_log_dash/Backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import MeView, CategoryListView, ProductListView, OrderCreateView
-
-urlpatterns = [
-    path("me/", MeView.as_view(), name="me"),
-    path("categories/", CategoryListView.as_view(), name="categories"),
-    path("products/", ProductListView.as_view(), name="products"),
-    path("orders/", OrderCreateView.as_view(), name="orders"),
-]
